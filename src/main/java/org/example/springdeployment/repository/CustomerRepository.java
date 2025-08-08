1cbba2d86b03f3b126fda213bf1b5cb3c78ee657
tharusenevirathne <tharusanduni33@gmail.com>
2025-08-09 00:13:52 +0530

    update

--- src/main/java/org/example/springdeployment/repository/CustomerRepository.java
+++ src/main/java/org/example/springdeployment/repository/CustomerRepository.java
@@ -2,13 +2,8 @@ package org.example.springdeployment.repository;
 
 import org.example.springdeployment.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-//    @Modifying
-//    @Query("update Customer c set c.name = ?1, c.email = ?2 where c.id = ?3")
-//    void update(String name, String email, Long id);
 }
